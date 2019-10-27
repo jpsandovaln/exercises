bc7f304e8928bd61c129975328c6461a10979856
Paolo Sandoval <paolosandovaln@gmail.com>
2019-10-27 12:20:47 -0400

    added unit test

---
+++ Problem2/src/main/java/com/problem2/geolocation/NullListException.java
@@ -0,0 +1,17 @@
+package com.problem2.geolocation;
+
+/**
+ * Null List exception.
+ * @author Paolo Sandoval
+ * @version 1.0
+ */
+public class NullListException extends Exception {
+
+    /**
+     * Constructor to create a new exception.
+     * @param message error message.
+     */
+    public NullListException(String message) {
+        super(message);
+    }
+}
