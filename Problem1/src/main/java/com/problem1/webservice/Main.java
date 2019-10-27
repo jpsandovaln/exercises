db986480aee49ad94cd720566a77254c674e205d
Paolo Sandoval <paolosandovaln@gmail.com>
2019-10-26 22:41:36 -0400

    Added Main class and properties file

---
+++ Problem1/src/main/java/com/problem1/webservice/Main.java
@@ -0,0 +1,19 @@
+package com.problem1.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Encapsulates the application's main entry point.
+ */
+@SpringBootApplication
+public class Main {
+
+    /**
+     * The application's main entry point.
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
