53aa6044d11191d942c5ca224f9ab6774b7ac595
Paolo Sandoval <paolosandovaln@gmail.com>
2019-10-27 09:56:37 -0400

    Added new project

---
+++ Problem2/src/main/java/com/problem2/geolocation/Main.java
@@ -0,0 +1,16 @@
+package com.problem2.geolocation;
+
+/**
+ * Encapsulates the application's main entry point.
+ * @author Paolo Sandoval
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * The application's main entry point.
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+    }
+}
